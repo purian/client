3066b24435c9a179538f53a43fdc6d4fbac5f1ba
purian <purian@gmail.com>
2015-07-25 19:23:40 +0300

    Update basic model attributes

--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+    id: DS.attr('integer'),
+    name: DS.attr('string'),
+    category: DS.attr('string'),
+    sku: DS.attr('string'),
+    createdAt: DS.attr('date'),
+    updatedAt: DS.attr('date')
 });
