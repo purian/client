117e2ed929b40faf1905c6781827bd026ad66984
purian <purian@gmail.com>
2015-07-25 22:18:05 +0300

    Show the products in a bootstrap table

--- app/routes/products/index.js
+++ app/routes/products/index.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend({
-    model: function() {
-        return this.store.findAll('product');
+
+export default Ember.Route.extend(RouteMixin, {
+    perPage: 25,
+
+    model: function(params) {
+        return this.findPaged('product',params);
     }
 });
